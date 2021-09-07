4b4b18040877d9737452dd1944996498749e18b9
byt3-m3 <cbaxtertech@gmail.com>
2021-09-07 17:51:59 -0400

    Added new test contstants

---
+++ src/tests/const.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+TEST_NAME = 'test_name'
+TEST_EMAIL = 'test@email.com'
+TEST_DESCRIPTION= 'test_description'
+TEST_DATETIME = datetime.strptime("2020 06 09", "%Y %d %m")

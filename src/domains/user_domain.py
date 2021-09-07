ef3e504851f15b541def940f6201457bedff66d3
byt3-m3 <cbaxtertech@gmail.com>
2021-09-07 10:44:36 -0400

    Added new test for aggregates

--- src/domains/user_domain.py
+++ src/domains/user_domain.py
@@ -2,18 +2,19 @@ from src.domains.core_domain import CoreAggregate
 from dataclasses import dataclass, field
 from src.models.user_models import (
     FamilyDataModel,
+    UserDataModel,
     ChildDataModel,
     AdultDataModel,
     RoleDataModel
 )
-from eventsourcing.domain import AggregateEvent
+from eventsourcing.domain import AggregateEvent, event
 from typing import List, Dict
 from uuid import UUID
 from src.enums import (
     PermissionsEnum
 
 )
-from collections import defaultdict
+
 
 @dataclass
 class RoleAggregate(CoreAggregate):
@@ -29,17 +30,24 @@ class RoleAggregate(CoreAggregate):
         permission: PermissionsEnum
 
         def apply(self, aggregate: 'RoleAggregate') -> None:
-            aggregate.model.permissions.append(self.permission)
+            if self.permission not in aggregate.model.permissions:
+                aggregate.model.permissions.append(self.permission)
+            else:
+                raise PermissionError(f"{self.permission} Already Present in Permissions")
 
     class RemovePermissionEvent(AggregateEvent):
         permission: PermissionsEnum
 
         def apply(self, aggregate: 'RoleAggregate') -> None:
-            aggregate.model.permissions.remove(self.permission)
+            if self.permission in aggregate.model.permissions:
+                aggregate.model.permissions.remove(self.permission)
+            else:
+                raise PermissionError(f"{self.permission} not in permission list")
 
 
 @dataclass
 class UserAccountAggregate(CoreAggregate):
+    model: UserDataModel
     role_mapping: Dict[UUID, RoleAggregate]
 
     def add_role(self, role: RoleAggregate):
@@ -54,17 +62,25 @@ class UserAccountAggregate(CoreAggregate):
             role=role
         )
 
-    class AddRoleEvent(AggregateEvent):
-        role: RoleAggregate
+    @event("ChangeFirstName")
+    def change_first_name(self, value):
+        self.model.first_name = value
 
-        def apply(self, aggregate: 'ChildAggregate') -> None:
-            aggregate.role_mapping[self.role.id] = self.role
+    @event("ChangeLastName")
+    def change_first_name(self, value):
+        self.model.last_name = value
 
-    class RemoveRoleEvent(AggregateEvent):
-        role: RoleAggregate
+    @event("ChangeEmail")
+    def change_first_name(self, value):
+        self.model.email = value
 
-        def apply(self, aggregate: 'ChildAggregate') -> None:
-            aggregate.role_mapping.pop(self.role.id)
+    @event("AddRoleEvent")
+    def add_role(self, role: RoleAggregate):
+        self.role_mapping[role.id] = role
+
+    @event("RemoveRoleEvent")
+    def remove_role(self, role: RoleAggregate):
+        self.role_mapping.pop(role.id)
 
 
 @dataclass
@@ -80,8 +96,14 @@ class AdultAggregate(UserAccountAggregate):
 @dataclass
 class FamilyAggregate(CoreAggregate):
     model: FamilyDataModel
-    children_mapping: Dict[UUID, 'ChildAggregate'] = field(default_factory=dict)
-    parent_mapping: Dict[UUID, 'AdultAggregate'] = field(default_factory=dict)
+    _children_mapping: Dict[UUID, 'ChildAggregate'] = field(default_factory=dict)
+    _parent_mapping: Dict[UUID, 'AdultAggregate'] = field(default_factory=dict)
+
+    def get_child(self, child: ChildAggregate):
+        return self._children_mapping.get(child.id)
+
+    def get_parent(self, parent: AdultAggregate):
+        return self._parent_mapping.get(parent.id)
 
     def add_child(self, child: ChildAggregate):
         self.trigger_event(
@@ -95,26 +117,38 @@ class FamilyAggregate(CoreAggregate):
             child=child
         )
 
+    def add_parent(self, parent: AdultAggregate):
+        self.trigger_event(
+            self.AddParentEvent,
+            parent=parent
+        )
+
+    def remove_parent(self, parent: AdultAggregate):
+        self.trigger_event(
+            self.RemoveParentEvent,
+            parent=parent
+        )
+
     class AddParentEvent(AggregateEvent):
         parent: AdultAggregate
 
         def apply(self, aggregate: 'FamilyAggregate') -> None:
-            aggregate.parent_mapping[self.parent.id] = self.parent
+            aggregate._parent_mapping[self.parent.id] = self.parent
 
     class RemoveParentEvent(AggregateEvent):
         parent: AdultAggregate
 
         def apply(self, aggregate: 'FamilyAggregate') -> None:
-            aggregate.parent_mapping.pop(self.parent.id)
+            aggregate._parent_mapping.pop(self.parent.id)
 
     class AddChildEvent(AggregateEvent):
         child: ChildAggregate
 
         def apply(self, aggregate: 'FamilyAggregate') -> None:
-            aggregate.children_mapping[self.child.id] = self.child
+            aggregate._children_mapping[self.child.id] = self.child
 
     class RemoveChildEvent(AggregateEvent):
         child: ChildAggregate
 
         def apply(self, aggregate: 'FamilyAggregate') -> None:
-            aggregate.children_mapping.pop(self.child.id)
+            aggregate._children_mapping.pop(self.child.id)
--- src/tests/test_user_domain.py
+++ src/tests/test_user_domain.py
@@ -1,3 +1,4 @@
+import pytest
 from pytest import fixture
 from src.domains.user_domain import (
     ChildAggregate,
@@ -6,10 +7,21 @@ from src.domains.user_domain import (
     AdultDataModel,
     FamilyDataModel,
     FamilyAggregate,
+    UserAccountAggregate,
+    UserDataModel,
     RoleAggregate
 )
 from src.models.user_models import RoleDataModel, PermissionsEnum
 from src.enums import GenderEnum
+
+
+@fixture
+def user_data_model():
+    return UserDataModel(
+        first_name='test_first_name',
+        last_name='test_last_name',
+        email="test_email"
+    )
 
 
 @fixture
@@ -62,20 +74,103 @@ def adult_aggregate_testable(role_aggregate_testable):
 
 
 @fixture
-def family_aggregate_testable(child_aggregate_testable, adult_aggregate_testable):
-    yield FamilyAggregate(
+def family_aggregate_testable(child_aggregate_testable, adult_aggregate_testable) -> FamilyAggregate:
+    return FamilyAggregate(
         model=FamilyDataModel(
             name="test_family_name")
+    )
 
+
+@fixture
+def user_account_aggregate_testable(role_aggregate_testable, user_data_model):
+    return UserAccountAggregate(
+        model=user_data_model,
+        role_mapping={
+            role_aggregate_testable.id: role_aggregate_testable
+        }
     )
 
 
 def test_family_aggregate_add_child(family_aggregate_testable, child_aggregate_testable):
     child_aggregate = child_aggregate_testable
-    family_aggregate = family_aggregate_testable
-    family_aggregate: FamilyAggregate
-
-    family_aggregate.add_child(child_aggregate)
-    result = family_aggregate.children_mapping.get(child_aggregate_testable.id)
-
+    family_aggregate_testable.add_child(child_aggregate)
+    result = family_aggregate_testable.get_child(child=child_aggregate)
     assert isinstance(result, ChildAggregate)
+
+
+def test_family_aggregate_remove_child(family_aggregate_testable, child_aggregate_testable):
+    child_aggregate = child_aggregate_testable
+    family_aggregate_testable.add_child(child_aggregate)
+    family_aggregate_testable.remove_child(child_aggregate)
+    result = family_aggregate_testable.get_child(child_aggregate_testable)
+    assert result is None
+
+
+def test_family_aggregate_add_parent(family_aggregate_testable, adult_aggregate_testable):
+    parent = adult_aggregate_testable
+    family_aggregate_testable.add_parent(parent)
+    result = family_aggregate_testable.get_parent(parent)
+    assert isinstance(result, AdultAggregate)
+
+
+def test_family_aggregate_remove_parent(family_aggregate_testable, adult_aggregate_testable):
+    parent = adult_aggregate_testable
+    family_aggregate_testable.add_parent(parent)
+    family_aggregate_testable.remove_parent(parent=parent)
+    result = family_aggregate_testable.get_parent(parent)
+    assert result is None
+
+
+def test_user_account_aggregate_add_role(user_account_aggregate_testable, role_data_model):
+    testable = user_account_aggregate_testable
+
+    new_role = RoleAggregate(model=role_data_model)
+    testable.add_role(new_role)
+
+    actual = testable.role_mapping.get(new_role.id)
+
+    assert isinstance(actual, RoleAggregate)
+
+
+def test_user_account_aggregate_remove_role(user_account_aggregate_testable, role_data_model):
+    testable = user_account_aggregate_testable
+
+    new_role = RoleAggregate(model=role_data_model)
+    testable.add_role(new_role)
+    testable.remove_role(role=new_role)
+
+    actual = testable.role_mapping.get(new_role.id)
+
+    assert actual is None
+
+
+def test_user_account_aggregate_change_first_name(user_account_aggregate_testable):
+    subject = user_account_aggregate_testable
+    subject.change_first_name("New Name")
+    print(subject.collect_events())
+    assert subject.model.first_name == "New Name"
+
+
+def test_role_aggregate_append_permission(role_aggregate_testable):
+    testable = role_aggregate_testable
+    testable.append_permission(permission=PermissionsEnum.AccountAdminModifyPermissions)
+    assert PermissionsEnum.AccountAdminModifyPermissions in testable.model.permissions
+
+
+def test_role_aggregate_append_permission_2(role_aggregate_testable):
+    testable = role_aggregate_testable
+    with pytest.raises(PermissionError):
+        testable.append_permission(permission=PermissionsEnum.ADMIN)
+
+
+def test_role_aggregate_remove_permission(role_aggregate_testable):
+    testable = role_aggregate_testable
+    testable.remove_permission(permission=PermissionsEnum.ADMIN)
+    assert PermissionsEnum.ADMIN not in testable.model.permissions
+    assert len(testable.model.permissions) == 0
+
+
+def test_role_aggregate_remove_permission_2(role_aggregate_testable):
+    testable = role_aggregate_testable
+    with pytest.raises(PermissionError):
+        testable.remove_permission(permission=PermissionsEnum.AccountAddAccountAdmin)

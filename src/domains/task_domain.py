ba654b6b76c77e04a8b4cee059adbf2fa244c97c
byt3-m3 <cbaxtertech@gmail.com>
2021-09-08 08:11:02 -0400

    Updated TaskDomain aggreates and tests

--- src/domains/task_domain.py
+++ src/domains/task_domain.py
@@ -3,53 +3,65 @@ from dataclasses import dataclass, field
 from typing import List
 
 from eventsourcing.domain import Aggregate, event
-from src.domains.user_domain import ChildAggregate
-from src.models.task_models import TaskDataModel, TaskItemDataModel
+from uuid import UUID
 
 
 @dataclass
 class TaskItem(Aggregate):
-    model: TaskItemDataModel
+    name: str
+    description: str
 
 
 @dataclass
 class TaskAggregate(Aggregate):
-    model: TaskDataModel
-    assignee: ChildAggregate
-    items: List[TaskItem] = field(default_factory=list)
+    name: str
+    description: str
+    assign_date: datetime
+    due_date: datetime
+    assignee: UUID
+    attachments: List[bytes] = field(default_factory=list)
+    items: List[UUID] = field(default_factory=list)
+    is_complete: bool = field(default=False)
 
     @event
-    def append_item(self, task_item: TaskItem):
-        if task_item not in self.items:
-            self.items.append(task_item)
+    def append_item(self, task_item_id: UUID):
+        if task_item_id not in self.items:
+            self.items.append(task_item_id)
 
     @event
-    def remove_item(self, task_item: TaskItem):
-        if task_item in self.items:
-            self.items.remove(task_item)
+    def remove_item(self, task_item_id: UUID):
+        if task_item_id in self.items:
+            self.items.remove(task_item_id)
 
     @event
-    def update_assignee(self, child: ChildAggregate):
-        self.assignee = child
+    def update_assignee(self, child_id: UUID):
+        self.assignee = child_id
         self.update_assign_date()
 
     @event
     def set_not_complete(self):
-        self.model.is_complete = False
+        if self.is_complete:
+            self.is_complete = False
 
     @event
     def set_complete(self):
-        if not self.model.is_complete:
-            self.model.is_complete = True
+        if not self.is_complete:
+            self.is_complete = True
 
     @event
     def update_assign_date(self):
-        self.model.assign_date = datetime.datetime.now()
+        self.assign_date = datetime.datetime.now()
 
     @event
-    def update_attachment(self, data: bytes):
-        self.model.attachment = data
+    def append_attachment(self, data: bytes):
+        if data not in self.attachments:
+            self.attachments.append(data)
+
+    @event
+    def remove_attachment(self, data: bytes):
+        if data in self.attachments:
+            self.attachments.remove(data)
 
     @event
     def update_due_date(self, date: datetime.datetime):
-        self.model.due_date = date
+        self.due_date = date
--- src/tests/test_task_domain.py
+++ src/tests/test_task_domain.py
@@ -2,80 +2,67 @@ from datetime import timedelta
 
 from pytest import fixture
 from src.domains.task_domain import TaskAggregate, TaskItem
-from src.models.task_models import TaskDataModel, TaskItemDataModel
 from src.utils.core_utils import clone_item
 
 
 @fixture
-def task_data_model(test_name, test_email, test_datetime, test_description):
-    return TaskDataModel(
+def task_aggregate(child_aggregate, test_name, test_email, test_datetime, test_description):
+    return TaskAggregate(
         name=test_name,
         description=test_description,
         assign_date=clone_item(test_datetime),
         due_date=clone_item(test_datetime),
-
+        assignee=child_aggregate.id
     )
 
 
 @fixture
-def task_item_data_model(test_name, test_description):
-    return TaskItemDataModel(
+def task_item_aggregate(test_name, test_description):
+    return TaskItem(
         name=test_name,
         description=test_description,
-
     )
 
 
-@fixture
-def task_aggregate(task_data_model, child_aggregate):
-    return TaskAggregate(
-        model=task_data_model,
-        assignee=child_aggregate
-    )
-
-
-@fixture
-def task_item_aggregate(task_item_data_model):
-    return TaskItem(
-        model=task_item_data_model
-    )
-
-
-def test_task_aggregate_update_child(task_aggregate, child_aggregate):
-    child = clone_item(child_aggregate)
-    child.update_first_name("updated_fname")
-    child.update_last_name("updated_lname")
-    task_aggregate.update_assignee(child=child)
+def test_task_aggregate_update_assignee(task_aggregate, child_aggregate):
+    task_aggregate.update_assignee(child_id=child_aggregate.id)
     events = task_aggregate.collect_events()
 
     assert isinstance(events[2], task_aggregate.UpdateAssignee)
     assert isinstance(events[1], task_aggregate.UpdateAssignDate)
-    assert task_aggregate.assignee is child
-    assert task_aggregate.assignee.first_name == "updated_fname"
+    assert task_aggregate.assignee is child_aggregate.id
 
 
 def test_task_aggregate_set_complete(task_aggregate):
-    assert task_aggregate.model.is_complete is False
+    assert task_aggregate.is_complete is False
     task_aggregate.set_complete()
-    assert task_aggregate.model.is_complete is True
+    assert task_aggregate.is_complete is True
 
 
 def test_task_aggregate_set_not_complete(task_aggregate):
     task_aggregate.set_complete()
-    assert task_aggregate.model.is_complete is True
+    assert task_aggregate.is_complete is True
     task_aggregate.set_not_complete()
-    assert task_aggregate.model.is_complete is False
+    assert task_aggregate.is_complete is False
 
 
-def test_task_aggregate_update_asign_date(task_aggregate, test_datetime):
-    assert task_aggregate.model.assign_date == test_datetime
+def test_task_aggregate_update_assign_date(task_aggregate, test_datetime):
+    assert task_aggregate.assign_date == test_datetime
     task_aggregate.update_assign_date()
-    assert task_aggregate.model.assign_date > test_datetime
+    assert task_aggregate.assign_date > test_datetime
 
 
-def test_task_aggregate_update_attachment(task_aggregate, test_byte_data):
-    task_aggregate.update_attachment(data=test_byte_data)
-    assert task_aggregate.model.attachment == test_byte_data
+def test_task_aggregate_append_attachment(task_aggregate, test_byte_data):
+    task_aggregate.append_attachment(data=test_byte_data)
+    assert test_byte_data in task_aggregate.attachments
+    assert len(task_aggregate.attachments) == 1
+
+
+def test_task_aggregate_remove_attachment(task_aggregate, test_byte_data):
+    task_aggregate.append_attachment(data=test_byte_data)
+    task_aggregate.remove_attachment(data=test_byte_data)
+    assert test_byte_data not in task_aggregate.attachments
+    assert len(task_aggregate.attachments) == 0
 
 
 def test_task_aggregate_update_due_date(task_aggregate, test_datetime):
@@ -83,14 +70,15 @@ def test_task_aggregate_update_due_date(task_aggregate, test_datetime):
     delta = timedelta(days=5)
     due_date = due_date + delta
     task_aggregate.update_due_date(date=due_date)
-    assert task_aggregate.model.due_date is due_date
+    assert task_aggregate.due_date is due_date
 
 
 def test_task_aggregate_append_item(task_aggregate, task_item_aggregate):
-    task_aggregate.append_item(task_item=task_item_aggregate)
+    task_aggregate.append_item(task_item_id=task_item_aggregate.id)
     assert len(task_aggregate.items) == 1
 
+
 def test_task_aggregate_remove_item(task_aggregate, task_item_aggregate):
-    task_aggregate.append_item(task_item=task_item_aggregate)
-    task_aggregate.remove_item(task_item=task_item_aggregate)
+    task_aggregate.append_item(task_item_id=task_item_aggregate.id)
+    task_aggregate.remove_item(task_item_id=task_item_aggregate.id)
     assert len(task_aggregate.items) == 0
